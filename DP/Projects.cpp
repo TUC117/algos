d6122a828a0312583dbf55d676b27709974f4be7
TUC117 <22b0913@iitb.ac.in>
2024-02-24 23:03:38 +0530

    Projects with failed submission

--- DP/Projects.cpp
+++ DP/Projects.cpp
@@ -11,6 +11,32 @@ struct object{
 bool compare(const object &a, const object &b) {
     return a.b1 < b.b1;
 }
+int getk(std::vector<object>& vec, int i) {
+    int left = i;
+    int target = vec[i].b1;
+    int right = vec.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        // If target greater, ignore left half
+        if (vec[mid].a1 <= target) {
+            left = mid + 1;
+        }
+        // If target is smaller or equal, ignore right half
+        else {
+            right = mid - 1;
+        }
+    }
+
+    // If we reach here, then left is the index of the a1 element greater than target
+    if (left < vec.size() && vec[left].a1 > target) {
+        return left;
+    }
+
+    // If no element is greater than target
+    return -1;
+}
 
 int main(){
     std::ios::sync_with_stdio(false);
@@ -28,19 +54,17 @@ int main(){
         auto it = std::upper_bound(vec.begin(), vec.end(), temp, compare);
         vec.insert(it, temp);
     }
-    // Now we have n in order of decreasing time.
     
-    int dp[n+1];
-    for(int i=n;i>=0;i--){
-        int 
-        dp[i] = dp[i+1] + dp[i-k];
+    std::vector<int> dp(n+1,INT_MIN);
+    for(int i=1;i<n+1;i++) dp[i] = vec[i-1].p;
+    
+    for(int i=n-1;i>=0;i--){
+        int k = getk(vec, i);
+        dp[i] = std::max(dp[i+1],dp[i]);
+        if(k!=-1 && i+k<n) dp[i] = std::max(dp[i+1], dp[i+1+k]+vec[i].p);
     }
-    // for(auto &i : vec){
-    //     std::cout<<i.a1<<" ";
-    //     std::cout<<i.b1<<" ";
-    //     std::cout<<i.p<<" ";
-    //     std::cout<<"\n";
-    // }
+    std::cout<<dp[0]<<"\n";
+    
 
     return 0;
 }
